5665f845ad0e9a08edaa30d021bc7708d0964ff2
Shrey Goel <120201124+SHR3YGO3L@users.noreply.github.com>
2023-12-08 01:12:31 +0530

    Update signup.php

--- signup.php
+++ signup.php
@@ -1,25 +1,45 @@
 <?php
-include 'db.php';
 
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    $username = $_POST['username'];
-    $password = $_POST['password'];
 
-    // Use prepared statement to prevent SQL injection
-    $stmt = $conn->prepare("INSERT INTO users( username, password) VALUES (?, ?)");
-    $stmt->bind_param("ss", $username, $password_hash);
+$email =$_POST['username'];
+$password =$_POST['password'];
+$con = new mysqli("database-1.c09sqyoza5hu.us-east-1.rds.amazonaws.com","root", "password", "userdb");
 
-    // Hash the password for security
-    $password_hash = password_hash($password, PASSWORD_DEFAULT);
+if($con->connect_error) {
 
-    if ($stmt->execute()) {
-        echo "User registered successfully.";
-    } else {
-        echo "Error: " . $stmt->error;
-    }
+die("Failed to connect : ".$con->connect_error);
 
-    $stmt->close();
+} else {
+
+$stmt = $con->prepare("select * from users where email = ?");
+
+$stmt->bind_param("s", $email);
+
+$stmt->execute();
+
+$stmt_result = $stmt->get_result();
+
+if($stmt_result->num_rows > 0) {
+
+$data = $stmt_result->fetch_assoc();
+
+I
+
+if($data['password'] === $password) {
+
+echo "<h2>Login Successfully</h2>";
+
+} else {
+
+echo "<h2>Invalid Email or password</h2>";
+
 }
 
-$conn->close();
+} else {
+
+echo "<h2>Invalid Email or password</h2>";
+
+}
+
+}
 ?>
